b13adec5a1f66e2aec0902b05ea92fd56d410b8c
Alessio Nardin <alessio.nardin@gmail.com>
2022-01-17 10:58:15 +0100

    finished templating

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,9 +19,13 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
-	/*mux.Get("/make-reservation", handlers.Repo.About)
-	mux.Get("/contact", handlers.Repo.About)
-	*/
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
+
+	mux.Get("/make-reservation", handlers.Repo.About)
+
+	//mux.Get("/contact", handlers.Repo.About)
+
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
