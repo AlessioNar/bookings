6c129c47431b476b5efb0a3d9ad588e4c62b272d
Alessio Nardin <alessio.nardin@gmail.com>
2022-01-20 11:34:41 +0100

    serverside validation part 3

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -3,6 +3,8 @@ package forms
 import (
   "net/http"
   "net/url"
+  "strings"
+  "fmt"
 )
 
 
@@ -24,13 +26,32 @@ func New(data url.Values) *Form {
   }
 }
 
+// Checks for required fields
+func (f *Form) Required(fields ...string) {
+  for _, field := range fields {
+    value := f.Get(field)
+    if strings.TrimSpace(value) == "" {
+      f.Errors.Add(field, "This field cannot be blank")
+    }
+  }
+}
+
 // Has checks if form fields is in post and it is not empty
 func (f *Form) Has(field string, r *http.Request) bool {
   x := r.Form.Get(field)
-  if x == "" {
-    f.Errors.Add(field, "This field cannot be blank")
+  if x == "" {    
     return false
   }
 
+  return true
+}
+
+// MinLength checks for string minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+  x := r.Form.Get(field)
+  if len(x) < length {
+    f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
+    return false
+  }
   return true
 }
